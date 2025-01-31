1e358fa70e6e55f9fbe7ef5e6b2d9c760d17df5d
Olga Tsiouri <olgatsiouri@outlook.com>
2025-02-01 01:19:29 +0200

    .

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # dos2unix-GUI
- A fast lightweight windows GUI wrapper for dos2unix
+A fast lightweight windows GUI wrapper for dos2unix
+
+## Installation
+1. Click to download `dos2unix-7.5.2-win64.zip` from this [link](https://sourceforge.net/projects/dos2unix/files/dos2unix/7.5.2/).
+2. unzip `dos2unix-7.5.2-win64.zip` using WinRAR or other related software.
+3. Move `does2unix.exe` from `dos2unix-7.5.2-win64\bin` to `C:\Windows\System32`.
+4. Dowload `dos2unix GUI.exe` from the `bin` directory.
+
+## Usage
+![](img/1.png)
+If the patterm option is left empty, when you browse, you will select an input file.
+If you use a pattern option like `*.txt` when you click browse you will select the folder your input files are located.
+**Note: This dos2unix GUI program creates a newline at the end of the file if it doesn't exist.**
---
+++ src/dos2unix GUI.py
@@ -0,0 +1,77 @@
+import os
+import threading
+import subprocess
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+
+def run_pipeline(input_file, pattern, progress_bar):
+
+    # Start the progress bar
+    progress_bar.start()
+
+    if pattern=="":
+        # Select input file
+        infile = os.path.basename(input_file)
+    
+    # Convert path to windows format
+    input_file_fixed = str(input_file).replace("/","\\")
+
+
+    # Change to the input file's directory
+    os.chdir(os.path.dirname(input_file)) if pattern=="" else os.chdir(input_file)
+
+    if pattern=="":
+        command = f"dos2unix -e \"{infile}\""
+    else:
+        command = f"dos2unix -e {pattern}"
+    
+    try:
+        subprocess.run(command, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
+        progress_bar.stop()
+        messagebox.showinfo("Success", f"{input_file_fixed} converted to unix format") if pattern=="" else messagebox.showinfo("Success", f"Files at {input_file_fixed} converted to unix format")
+
+    except subprocess.CalledProcessError as e:
+        progress_bar.stop()
+        print(f"Error: {e}")
+        
+def start_thread():
+    input_file = input_file_var.get()
+    pattern = pattern_var.get()
+
+
+    if not input_file:
+        messagebox.showwarning("Input Error", "Please select an input file.")
+        return
+    
+    # Start command in a new thread
+    thread = threading.Thread(target=run_pipeline, args=(input_file,  pattern, progress_bar))
+    thread.start()
+
+def select_operation():
+    pattern = pattern_var.get()
+    file_path = filedialog.askopenfilename() if pattern == "" else filedialog.askdirectory()
+    input_file_var.set(file_path)
+
+# Set up tkinter app
+app = tk.Tk()
+app.title("dos2unix GUI")
+
+# Pattern
+tk.Label(app, text="Pattern of files to convert:").grid(row=0, column=0, padx=10, pady=10, sticky="e")
+pattern_var = tk.StringVar(value="")
+tk.Entry(app, textvariable=pattern_var, width=40).grid(row=0, column=1, padx=10, pady=10, sticky="w")
+
+# Input file selection
+input_file_var = tk.StringVar()
+tk.Label(app, text="Input File or Folder:").grid(row=1, column=0, padx=10, pady=10, sticky="e")
+tk.Entry(app, textvariable=input_file_var, width=40).grid(row=1, column=1, padx=10, pady=10)
+tk.Button(app, text="Browse", command=select_operation).grid(row=1, column=2, padx=10, pady=10)
+
+# Progress Bar (indeterminate)
+progress_bar = ttk.Progressbar(app, mode="indeterminate", length=200)
+progress_bar.grid(row=2, column=0, columnspan=3, padx=10, pady=20)
+
+# Start button
+tk.Button(app, text="Run program", command=start_thread).grid(row=3, column=1, padx=10, pady=20)
+
+app.mainloop()
